f2cfa99eca11d2777228caf9a681c2be67a8e556
Jerome <jerome.pihet@viacesi.fr>
2019-11-06 13:16:49 +0100

    First push, structure initialisation

---
+++ register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/vendors/fontawesome-free-5.11.1-web/css/all.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Site BDE Inscription</title>
+    </head>
+
+    <body>
+
+    <!-- L'en-tête -->    
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <img src="assets/img/site/cesi_logo.png" alt="Logo du cesi" height=100px >
+                <h1 class="col-md-8 ml-auto">Site du BDE</h1>
+            </div>
+        </div>
+    </header>
+
+    
+
+    <main>
+        <?php include('php/menu.php');?>
+
+        
+    </main>
+</body>
+
+</html>
