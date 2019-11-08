5e004874b486eea6d43395ecc6ede334cee61188
sofiane51520 <45100006+sofiane51520@users.noreply.github.com>
2019-11-08 11:37:17 +0100

    creation of the node api

---
+++ restweb/db_connection.js
@@ -0,0 +1,18 @@
+export function database(request){
+    let mysql = require('mysql');
+    let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bde"
+    });
+
+    con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(request, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+    });
+}
