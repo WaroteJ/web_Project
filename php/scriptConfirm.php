38bf1a983a75f2461d6d8bab7d014c4b3f10e789
Florian <florian.etchart@viacesi.fr>
2019-11-12 16:37:57 +0100

    confirm page with automatism in bdd finish

--- php/scriptConfirm.php
+++ php/scriptConfirm.php
@@ -1,3 +1,37 @@
 <?php
-    echo date('Y-m-d');
+    require("bdd.php");
+
+    if(isset($_SESSION["user"])){
+        $date = date('Y-m-d');
+        $id_User = $_SESSION["user"];
+        $requete = $bdd->prepare("INSERT INTO commande (date, id_User) VALUES (:date, :id_User)");
+        // Liaison des variables de la requête préparée aux variables PHP
+        $requete->bindValue(':date', $date, PDO::PARAM_STR);
+        $requete->bindValue(':id_User', $id_User, PDO::PARAM_INT);
+        //execution de la requête
+        $requete->execute();
+
+        $requete2 = $bdd->prepare("SELECT user_article.id as id_article, commande.id as id_commande, user_article.qte as qte FROM user_article INNER JOIN commande ON user_article.id_User = commande.id_User WHERE commande.id_User = :id_User");
+        // Liaison des variables de la requête préparée aux variables PHP
+        $requete2->bindValue(':id_User', $id_User, PDO::PARAM_INT);
+        //execution de la requête
+        $requete2->execute();
+
+        while($result = $requete2->fetch()):
+            //echo $result['id_article'];
+            $requete3 = $bdd->prepare("INSERT INTO article_commande (id, id_Commande, qte) VALUES (:id_art, :id_commande, :qte)");
+            // Liaison des variables de la requête préparée aux variables PHP et excution de la requete
+            $requete3->execute(array(
+                ':id_art'=>$result['id_article'],
+                ':id_commande'=>$result['id_commande'],
+                ':qte'=>$result['qte']
+            ));
+        endwhile;
+
+        $requete4 = $bdd->prepare("DELETE FROM user_article WHERE id_User = :id_User");
+        // Liaison des variables de la requête préparée aux variables PHP
+        $requete4->bindValue(':id_User', $id_User, PDO::PARAM_INT);
+        //execution de la requête
+        $requete4->execute();
+    }
 ?>
