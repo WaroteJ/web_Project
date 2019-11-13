ecbab16576e2d4fcfb92e0dce6e1bce55d88c69d
Jerome <jerome.pihet@viacesi.fr>
2019-11-13 13:46:33 +0100

    Made a script to return an event's entrants in a csv

---
+++ php/entrants.php
@@ -0,0 +1,23 @@
+<?php
+    require_once("bdd.php");
+    $list=array();
+
+    $req = $bdd->prepare("SELECT user.nom,user.prenom
+    FROM user LEFT JOIN event_user ON user.id = event_user.id_User
+    WHERE event_user.id=:id");
+    $req->execute(array(
+        ':id'=>3
+    ));
+
+    while($result = $req->fetch(PDO::FETCH_BOTH)){
+        $list[]=array($result[0],$result[1]);
+        }  
+    $req->closeCursor();
+
+    $fp = fopen('participants.csv','w');
+    fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
+    foreach ($list as $personne) {
+        fputcsv($fp,$personne);
+    }
+    fclose($fp);
+?>
