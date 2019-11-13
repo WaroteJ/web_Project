0e9926a0fe1f47298d359ecc62d92698ed810c77
Jerome <jerome.pihet@viacesi.fr>
2019-11-13 13:06:50 +0100

    Picture upload and file path in the database

--- php/bo/newArticle.php
+++ php/bo/newArticle.php
@@ -1,34 +1,45 @@
 <?php
+    $error=NULL;
 
     if(isset($_POST['newArticle'])){
-        $prix=0;
-        $descri='';
-        $photo='';
-        if(isset($_POST['prix'])){
-            if($_POST['prix']>=0)
-                $prix=inputSecure($_POST['prix']);
+        include ("php/image.php");
+        $photoArray=imageUpload("assets/img/products/");
+
+        if($photoArray[0]==1){
+            $prix=0;
+            $descri='';
+            $photo='';
+            if(isset($_POST['prix'])){
+                if($_POST['prix']>=0)
+                    $prix=inputSecure($_POST['prix']);
+            }
+            if(isset($_POST['descri'])){
+                $descri=inputSecure($_POST['descri']);
+            }
+
+            $requete = $bdd->prepare("INSERT INTO `article`(`nom_article`, `description`, `prix`, `id_Categorie`, `url`, `id_centre`) 
+            VALUES (:nom,:description,:prix,:cat,:photo,:id)");
+            $requete->execute(array(
+                ':id'=>$_SESSION['centre'],
+                ':prix'=>$prix,
+                ':nom'=>inputSecure($_POST['nom']),
+                ':description'=>$descri,
+                ':photo'=>$photoArray[1],
+                ':cat'=>inputSecure($_POST['cat'])
+            ));
+            $requete->closeCursor();
         }
-        if(isset($_POST['descri'])){
-            $descri=inputSecure($_POST['descri']);
+        else{
+            $error=1;
         }
-        if(isset($_POST['photo'])){
-            $photo=inputSecure($_POST['photo']);
-        }
-        $requete = $bdd->prepare("INSERT INTO `article`(`nom_article`, `description`, `prix`, `id_Categorie`, `url`, `id_centre`) 
-        VALUES (:nom,:description,:prix,:cat,:photo,:id)");
-        $requete->execute(array(
-            ':id'=>$_SESSION['centre'],
-            ':prix'=>$prix,
-            ':nom'=>inputSecure($_POST['nom']),
-            ':description'=>$descri,
-            ':photo'=>$photo,
-            ':cat'=>inputSecure($_POST['cat'])
-        ));
-        $requete->closeCursor();
     }
 ?>
-<form action="" method="post" class="whole_form">
+<form action="" method="post" class="whole_form" enctype="multipart/form-data">
     <h2>Ajouter un article</h2>
+    <?php if ($error!=NULL) {
+        echo '<h3>Image incorrecte ou déjà existante</h3>';
+        $error=NULL;
+    }?>
     <label for="nom">Nom de l'article</label>
     <input type="text" name="nom" id="nom" placeholder="Nom de l'article" required>
     <label for="cat">Catégorie</label> 
@@ -46,7 +57,7 @@
     <input type="number" name="prix" id="prix" placeholder="Prix" min="0">
     <label for="descri">Description de l'article</label>
     <textarea name="descri" id="descri" placeholder="Description de l'article" row=5></textarea>
-    <label for="logo">Photo de l'article</label>
-    <input type="text" name="photo" id="photo" placeholder="Url de la photo de l'article">
+    <label for="img">Photo de l'article</label>
+    <input type="file" name="img" id="img" accept="image/*">
     <input type="submit" value="Créer" name="newArticle">
 </form>
--- php/bo/newEvent.php
+++ php/bo/newEvent.php
@@ -1,40 +1,50 @@
 <?php
+    $error=NULL;
 
-    if(isset($_POST['newEvent'])){ 
-        $pay=1;
-        $prix=0;
-        $descri='';
-        $logo='';
-        if(isset($_POST['prix'])){
-            if($_POST['prix']>=0)
-                $prix=inputSecure($_POST['prix']); //Si le prix est >0 
-        }
-        if(!isset($_POST['payant'])){ // Si la case payant n'est pas coché, on set payant et prix à 0
-            $pay=0;             
+    if(isset($_POST['newEvent'])){
+        include ("php/image.php");
+        $photoArray=imageUpload("assets/img/events/");
+
+        if($photoArray[0]==1){
+            $pay=1;
             $prix=0;
+            $descri='';
+            if(isset($_POST['prix'])){
+                if($_POST['prix']>=0)
+                    $prix=inputSecure($_POST['prix']); //Si le prix est >0 
+            }
+            if(!isset($_POST['payant'])){ // Si la case payant n'est pas coché, on set payant et prix à 0
+                $pay=0;             
+                $prix=0;
+            }
+            if(isset($_POST['descri'])){
+                $descri=inputSecure($_POST['descri']);
+            }
+
+            $requete = $bdd->prepare("INSERT INTO `event`(`payant`, `prix`, `nom`, `description`, `date`, `logo`, `id_centre`) 
+            VALUES (:payant,:prix,:nom,:description,:date,:logo,:id)");
+            $requete->execute(array(
+                ':id'=>$_SESSION['centre'],
+                ':payant'=>$pay,
+                ':prix'=>$prix,
+                ':nom'=>inputSecure($_POST['nom']),
+                ':date'=>inputSecure($_POST['date']),
+                ':description'=>$descri,
+                ':logo'=>$photoArray[1],
+            ));
+            $requete->closeCursor();
         }
-        if(isset($_POST['descri'])){
-            $descri=inputSecure($_POST['descri']);
+        else{
+            $error=1;
         }
-        if(isset($_POST['logo'])){
-            $logo=inputSecure($_POST['logo']);
-        }
-        $requete = $bdd->prepare("INSERT INTO `event`(`payant`, `prix`, `nom`, `description`, `date`, `logo`, `id_centre`) 
-        VALUES (:payant,:prix,:nom,:description,:date,:logo,:id)");
-        $requete->execute(array(
-            ':id'=>$_SESSION['centre'],
-            ':payant'=>$pay,
-            ':prix'=>$prix,
-            ':nom'=>inputSecure($_POST['nom']),
-            ':date'=>inputSecure($_POST['date']),
-            ':description'=>$descri,
-            ':logo'=>$logo,
-        ));
-        $requete->closeCursor();
     }
 ?>
-<form class="add_event whole_form" action="" method="post" >
+<form class="add_event whole_form" action="" method="post" enctype="multipart/form-data">
     <h2>Création d'un évènement</h2>
+    <?php if ($error!=NULL) {
+        echo '<h3>Image incorrecte ou déjà existante</h3>';
+        $error=NULL;
+    }?>
     <label for="nom">Nom de l'évènement</label>
     <input type="text" name="nom" id="nom" placeholder="Nom de l'évènement" required maxlength="50">
     <label for="payant">Payant </label>
@@ -44,7 +54,7 @@
     <textarea name="descri" id="descri" placeholder="Description de l'évènement" row=5></textarea>
     <label for="date">Date de l'évènement</label>
     <input type="date" name="date" id="date" min="<?php echo date('Y-m-d');?>" required>
-    <label for="logo">Lien de l'affiche de l'évènement</label>
-    <input type="text" name="logo" id="logo" placeholder="Url du logo de l'évènement">
+    <label for="img">Affiche de l'évènement</label>
+    <input type="file" name="img" id="img" accept="image/*">
     <input type="submit" value="Créer" name="newEvent">
 </form>
