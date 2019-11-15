40230d935e740b3bfeb6d60014299c8ae81220f7
Jerome <jerome.pihet@viacesi.fr>
2019-11-15 11:43:13 +0100

    Title and h1 updated

--- articles.php
+++ articles.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="assets/css/article.css">
   <link rel="stylesheet" href="assets/css/style.css">
 
-  <title>Article</title>
+  <title>BDE CESI <?php echo $_SESSION['nomCentre']?> Article</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -21,7 +21,7 @@
         <div class="container-fluid">
             <div class="row">
             <img src="assets/img/site/cesi_logo.png" alt="Logo du cesi" height=100px >
-                <h1 class="col-md-8 ml-auto">Boutique du BDE</h1>
+                <h1 class="col-md-8 ml-auto">Boutique du BDE <?php echo $_SESSION['nomCentre']?></h1>
             </div>
         </div>
     </header>
