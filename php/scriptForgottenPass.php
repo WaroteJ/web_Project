5bce57b21d2917c796e60e11c0041f059c2bf942
Jerome <jerome.pihet@viacesi.fr>
2019-11-18 22:51:08 +0100

    Password recovery

--- php/scriptForgottenPass.php
+++ php/scriptForgottenPass.php
@@ -29,7 +29,7 @@
     }
     function randomPassword() {
         $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-        $pass = array(); //remember to declare $pass as an array
+        $pass = array(); 
         $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
         for ($i = 0; $i < 8; $i++) {
             $n = rand(0, $alphaLength);
